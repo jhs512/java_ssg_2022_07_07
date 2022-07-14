a44cef8cad4e730407fc3fcaea88de87361d4f8d
jhs512 <jangka512@gmail.com>
2022-07-14 15:08:24 +0900

    테스트 클래스 분리, FileDBTest에서 매 테스트 전에 test_data 폴더 삭제

--- src/main/java/com/ll/exam/Util.java
+++ src/main/java/com/ll/exam/Util.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -69,6 +72,18 @@ public class Util {
 
         return map;
     }
+
+    public static void deleteDir(String path) {
+        Path rootPath = Paths.get(path);
+        try (Stream<Path> walk = Files.walk(rootPath)) {
+            walk.sorted(Comparator.reverseOrder())
+                    .map(Path::toFile)
+                    .forEach(File::delete);
+        } catch (IOException e) {
+
+        }
+    }
+
 }
 
 class Pair {
